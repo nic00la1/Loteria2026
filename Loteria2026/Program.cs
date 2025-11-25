d17069c1bd9b2043624cd8f68d7e21b5fe4ff1f8
Nicola <nic00la.kaleta@gmail.com>
2025-11-25 21:13:34 +0100

    poprawiono dzialanie

--- Loteria2026/Program.cs
+++ Loteria2026/Program.cs
@@ -8,31 +8,35 @@ namespace Loteria2026
         static void Main(string[] args)
         {
             Console.WriteLine("Ile wygenerować losowań?");
+            WyswietlWyniki();
+        }
+
+        static void WyswietlWyniki()
+        {
             string ileLosowan = Console.ReadLine();
             int ileLosowanInt = int.Parse(ileLosowan);
 
-            Console.WriteLine("Zestawy wylosowanych liczb: ");
             int i = 1;
             while (i <= ileLosowanInt)
             {
                 Console.WriteLine($"Losowanie {i}: {string.Join(" ", Losuj6Liczb())}");
                 i++;
-                
-            }
-
-            static List<int> Losuj6Liczb()
-            {
-                Random random = new Random();
-                HashSet<int> liczby = new HashSet<int>(); // Dzięki HashSet - W jednym zestawie liczby nie mogą się powtarzać.
-
-                while (liczby.Count < 6)
-                {
-                    int liczba = random.Next(1, 49);
-                    liczby.Add(liczba);
-                }
-
-                return new List<int>(liczby);
             }
         }
+
+        static List<int> Losuj6Liczb()
+        {
+            Random random = new Random();
+            HashSet<int> liczby = new HashSet<int>(); // Dzięki HashSet - W jednym zestawie liczby nie mogą się powtarzać.
+
+            while (liczby.Count < 6)
+            {
+                int liczba = random.Next(1, 50);
+                liczby.Add(liczba);
+            }
+
+            return new List<int>(liczby);
+        }
+
     }
 }
