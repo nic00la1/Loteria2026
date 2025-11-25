1a9c8302aa19a7c528bef9b39022ad9fed06491a
nic00la1 <NicolaKaleta@TE-LOD-D-06S209>
2025-11-25 11:17:24 +0100

    init

--- Loteria2026/Program.cs
+++ Loteria2026/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Loteria2026
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ile wygenerować losowań?");
+            string ileLosowan = Console.ReadLine();
+
+            Console.WriteLine("Zestawy wylosowanych liczb: ");
+        }
+    }
+}
